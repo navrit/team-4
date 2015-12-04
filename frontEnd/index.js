e6718b555a5c8b7b98e8f071096702b3ed3b3a0e
Luke Sheard <me@lukesheard.com>
2015-12-04 22:10:41 +0000

    Server

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express     = require('express');
 
-var app = express();
+var app         = express();
+var server      = require('http').Server(app);
+var io          = require('socket.io')(server);
 
-app.get('/', function(req, res) {
+var port = process.port || 8080;
+
+server.listen(port, function() {
+    console.log("Server Live");
+});
+
+app.use(express.static(__dirname + '/static'))
+app.use('*', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 });
 
-app.listen(8080);
+io.on('connection', function (socket) {
+    socket.emit('news', {
+        hello: 'world'
+    });
+
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
