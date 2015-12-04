17a3f6b0fdb60c6dd2774af0b113d67bf503727b
Luke Sheard <me@lukesheard.com>
2015-12-04 23:05:24 +0000

    THIGNS

---
+++ frontEnd/src/app/app.js
@@ -0,0 +1,12 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 23.51033,
+            lng: 90.24176
+        },
+        zoom: 8
+    });
+
+    // INSERT YOUR CODE HERE
+}
