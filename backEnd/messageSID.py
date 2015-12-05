66c0de8ab92f562030f180a16e1929171f57641a
Navrit Bal <navritbal@gmail.com>
2015-12-05 07:08:40 +0000

    Just needs the parsing from Ario/Jacek

--- backEnd/messageSID.py
+++ backEnd/messageSID.py
@@ -1,30 +1,58 @@
-from twilio.rest import TwilioRestClient 
- 
-# account credentials here 
-ACCOUNT_SID = "AC7dc57255a855140dc9c2552589345e81" 
-AUTH_TOKEN = "5376c083003cc2ba3e06b2d9ef9ea636" 
- 
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
- 
-messages = client.messages.list() 
+from twilio.rest import TwilioRestClient
+import MySQLdb as mdb
+import datetime
 
-#Open filestream to current messagesSID.txt file.
-oldSID = open("messageSID.txt", "r")
+# Account credentials here
+ACCOUNT_SID = "AC7dc57255a855140dc9c2552589345e81"
+AUTH_TOKEN = "5376c083003cc2ba3e06b2d9ef9ea636"
 
-firstSID = oldSID.readline()
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+messages = client.messages.list()
 
-#Iterate through twilio's list of messageSIDs.
-for newSID in messages:
-	if((newSID.sid+"\n") != firstSID):#If there's a new one, print it
-		print newSID.body
-	else:
-		break
+def fileHandling():
+    # Open filestream to current messagesSID.txt file.
+    oldSID = open("messageSID.txt", "r")
 
+    firstSID = oldSID.readline()
 
-writer = open("messageSID.txt", "w") #Update our now out of date messages list.
+    # Iterate through twilio's list of messageSIDs.
+    for newSID in messages:
+    	if((newSID.sid+"\n") != firstSID):# If there's a new one, call parse and inject functions
+    		inject(newSID.body)
+    	else:
+    		pass
 
-for m in messages: 
-	writer.write(m.sid)
-	writer.write("\n")
+    writer = open("messageSID.txt", "w") # Update our now out of date messages list.
 
-writer.close()
+    for m in messages:
+    	writer.write(m.sid)
+    	writer.write("\n")
+
+    writer.close()
+
+def parse(text):
+    # PARSE IT WITH REGEX >> ars;23;Uganda..... --> Name:ars Age:23 Location:Uganda
+    
+    return time, name, age, location, phone, issuse, condtype
+
+def inject(text):
+    db = mdb.connect('127.0.0.1','root','jpmorgan','codeforgood')
+    cursor = db.cursor()
+    # Parse then inject into database
+    print text
+    time, name, age, location, phone, issue, condtypes = parse(text)
+    ''' SAMPLE DATA
+    time = "2015-12-03T21:10:29.000Z"
+    time = datetime.datetime.strptime( time, "%Y-%m-%dT%H:%M:%S.000Z" ) # Quick fix LOOK HERE - you need to parse this out for MySQL - no Timezone shizzle
+    # Goes to a native Python datetime, lookup the format (it's very human readable)
+    name = "Augustus"
+    age = "14"
+    location = "Uganda High Commission, Nairobi, Kenya"
+    phone = "254204445420"
+    issue = "No braille access at work"
+    condtype = "Visually impaired, wheelchair" '''
+    # Inject into database
+    query = "INSERT INTO codeforgood.data VALUES('{}','{}','{}','{}','{}','{}','{}');".format(time, name, age, location, phone, issue, condtype)
+    cursor.execute(query)
+    db.commit()
+    print ">> SUCCESS: New data in DB"
