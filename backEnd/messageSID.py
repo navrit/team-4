8239db7245368b5d4ea647cd00f1ec866b86bed2
Navrit Bal <navritbal@gmail.com>
2015-12-05 11:33:14 +0000

    Commenting

--- backEnd/messageSID.py
+++ backEnd/messageSID.py
@@ -1,39 +1,45 @@
 from twilio.rest import TwilioRestClient
 import MySQLdb as mdb
-import datetime
 import re
 
-# Account credentials here
+# Twilio account credentials
 ACCOUNT_SID = "AC7dc57255a855140dc9c2552589345e81"
 AUTH_TOKEN = "5376c083003cc2ba3e06b2d9ef9ea636"
 
+# Connect to Twilio's RESTful API with authentication
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+# Get all messages from Twilio
 messages = client.messages.list()
 
 def fileHandling():
-    # Open filestream to current messagesSID.txt file.
+    # Open filestream to last messagesSID.txt file - last list of messages
     oldSID = open("messageSID.txt", "r")
 
+    # Get 1st line of the old message ID list
     firstSID = oldSID.readline()
 
-    # Iterate through twilio's list of messageSIDs.
+    # Iterate through Twilio's list of messageSIDs
     for newSID in messages:
-    	if((newSID.sid+"\n") != firstSID):# If there's a new one, call parse and inject functions
-    		inject(newSID.body)
+    	if((newSID.sid+"\n") != firstSID):
+    		inject(newSID.body) # If there's a new one, call parse and inject functions
     	else:
     		break
 
-    writer = open("messageSID.txt", "w") # Update our now out of date messages list.
+    # Update our now out of date messages list
+    writer = open("messageSID.txt", "w")
 
     for m in messages:
     	writer.write(m.sid)
     	writer.write("\n")
 
-    writer.close()
+    try:
+        writer.close() # Safely close the file
+    except IOException as e:
+        print ">> IO ERROR" 
 
 def parse(text):
     # PARSE IT WITH REGEX >> ars;23;Uganda..... --> Name:ars Age:23 Location:Uganda
-    
+
     dataList = re.split(r';{1,}', text) #Puts text into a list where each element is seperated by ;
     dataList = [re.sub(r'\n', ' ', attributes) for attributes in dataList] #Gets rid of unwanted newline chars.
 
