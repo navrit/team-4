9dc99fa7ed7fcd0caeab3b684a83262a7f4eecb9
Luke Sheard <me@lukesheard.com>
2015-12-05 05:18:19 +0000

    stuff

--- backEnd/run.py
+++ backEnd/run.py
@@ -11,31 +11,31 @@ AUTH_TOKEN = "5376c083003cc2ba3e06b2d9ef9ea636"
 
 @app.route("/")
 def start():
-  return "Hello World!"
+    return "Hello World!"
 
 @app.route('/query', methods=['GET'])
 def query():
-  query = 'SELECT * FROM codeforgood.data'
-  sql = SQLObject(query)
-  return str(sql.get())
+    query = 'SELECT * FROM codeforgood.data'
+    sql = SQLObject(query)
+    return str(sql.get())
 
 @app.route('/queryfrom/<time>', methods=['GET'])
 def queryfrom(time):
-  query = 'SELECT * FROM codeforgood.data WHERE time > "%s"' % time
-  sql = SQLObject(query)
-  return str(sql.get())
+    query = 'SELECT * FROM codeforgood.data WHERE time > "%s"' % time
+    sql = SQLObject(query)
+    return str(sql.get())
 
-@app.route('/sms', methods=['POST'])
+@app.route('/sms', methods=['GET'])
 def sms():
-    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-
-    messages = client.messages.list()
-
-    print messages
+    # client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+    #
+    # messages = client.messages.list()
+    #
+    # print messages
 
     resp = twilio.twiml.Response()
     resp.message("Hello, Mobile Monkey")
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8888)
+    app.run(host='0.0.0.0', port=8080)
