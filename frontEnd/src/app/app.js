54e24daf7861d6966e0478ca27e323bfdba52b42
kaylashapiro <kshapi17@gmail.com>
2015-12-05 00:00:04 +0000

    Markers

--- frontEnd/src/app/app.js
+++ frontEnd/src/app/app.js
@@ -1,12 +1,23 @@
-var GoogleMapsLoader = require('google-maps');
-var map;
+var example = require('./example.js');
+console.log(example);
 
+var map;
+var GoogleMapsLoader = require('google-maps');
 GoogleMapsLoader.load(function(google) {
+
+  var myLatLng = {
+    lat: 23.51033,
+    lng: 90.24176
+  };
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 23.51033,
-            lng: 90.24176
-        },
+        center: myLatLng,
         zoom: 8
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World'
     });
 });
