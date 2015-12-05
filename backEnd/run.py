47e92b8cc70fccf109607557ab27e7c69681eb13
root <root@ip-10-37-147-132.eu-west-1.compute.internal>
2015-12-05 02:57:11 +0000

    bigger example

--- backEnd/run.py
+++ backEnd/run.py
@@ -10,9 +10,18 @@ def query():
   f = open('example.json', 'r')
   return f.read()
 
+@app.route('/queryfrom/<time>', methods=['GET'])
+def queryfrom(time):
+  return time
+
 @app.route('/sms', methods=['POST'])
 def sms():
   return "sms"
+
+@app.route('/example', methods=['GET'])
+def example():
+  f = open('example.json')
+  return f.read()
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
