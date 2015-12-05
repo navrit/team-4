277d47c430fcd943aec1d9408fa27c55f70c56c7
Luke Sheard <me@lukesheard.com>
2015-12-05 04:10:34 +0000

    lean up

--- backEnd/run.py
+++ backEnd/run.py
@@ -16,18 +16,11 @@ def query():
 def queryfrom(time):
   query = 'SELECT * FROM codeforgood.data WHERE time > "%s"' % time
   sql = SQLObject(query)
-  print query
-  print (sql.get())
   return str(sql.get())
 
 @app.route('/sms', methods=['POST'])
 def sms():
   return "sms"
 
-@app.route('/example', methods=['GET'])
-def example():
-  f = open('example.json')
-  return f.read()
-
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8888)
