4c67003a507ca5204aa11b7b0809b8fc6e8047b3
Navrit Bal <navritbal@gmail.com>
2015-12-05 00:40:10 +0000

    Gets all data and prints to screen

--- backEnd/getSQL.py
+++ backEnd/getSQL.py
@@ -6,28 +6,14 @@ class SQLObject:
     def __init__(self):
         self.db = mdb.connect('127.0.0.1','root','jpmorgan','data')
         self.cursor = self.db.cursor()
-        print getStations()
-
+        df = pd.read_sql("SELECT * FROM codeforgood.data;", con=self.db)
+        print df
     def __del__(self):
         self.close()
     def __exit__(self):
         self.close()
     def close(self):
         self.db.close()
-
-    '''def inject(self,data):
-        print ">> INJECTING "
-        data = self.parsePacket(data)'''
-
-    '''def parsePacket(self,data):
-        data = data.split(',') # Commas are our delimiters for the message '''
-
-    def getStations(self):
-        df = pd.read_sql(
-            "SELECT * \
-            FROM data.data;",
-            con=self.db)
-        return df
 
 if __name__ == "__main__":
     sql = SQLObject()
